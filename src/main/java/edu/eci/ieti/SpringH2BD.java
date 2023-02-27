cc4436ca9e91ba586b4467437fa67d6a4a1850d2
JuanesCortes <juanes9027@gmail.com>
2023-02-26 19:27:39 -0500

    primer commit

--- src/main/java/edu/eci/ieti/SpringH2BD.java
+++ src/main/java/edu/eci/ieti/SpringH2BD.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IetiApplication {
+public class SpringH2BD {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IetiApplication.class, args);
+		SpringApplication.run(SpringH2BD.class, args);
 	}
 
 }
